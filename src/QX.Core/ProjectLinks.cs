dca6ee7d13490b7b2e6db30febe5e75aab31ba7c
QDave <davidav08@gmail.com>
2026-09-06 12:33:12 +0200

    fix report bug

--- src/QX.Core/ProjectLinks.cs
+++ src/QX.Core/ProjectLinks.cs
@@ -2,7 +2,7 @@ namespace Qx;
 
 public static class ProjectLinks
 {
-    public const string Repository = "QX-Scripter/QX-Scripter";
+    public const string Repository = "QDaves/QX-Scripter";
     public static Uri Releases { get; } = new($"https://github.com/{Repository}/releases");
     public static Uri ReleaseApi { get; } = new($"https://api.github.com/repos/{Repository}/releases?per_page=100");
     public static Uri NewIssue { get; } = new($"https://github.com/{Repository}/issues/new");
